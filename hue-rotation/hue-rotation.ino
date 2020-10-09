ea76db08fba782b051c31737b6264f41361662ac
ninofiliu <nino.filiu@gmail.com>
2020-10-10 00:17:25 +0200

    hue rotation

---
+++ hue-rotation/hue-rotation.ino
@@ -0,0 +1,41 @@
+#include <DmxSimple.h>
+
+void write_rgb(int r, int g, int b) {
+  DmxSimple.write(1, r);
+  DmxSimple.write(2, g);
+  DmxSimple.write(3, b);
+}
+
+void write_hl(float h, float l) {
+  float r = 0;
+  float g = 0;
+  float b = 0;
+  if (h < 1. / 3.) {
+    r = 1. - 3. * h;
+    g = 3. * h;
+  }
+  if (h >= 1. / 3. && h < 2. / 3.) {
+    g = 2. - 3. * h;
+    b = -1. + 3. * h;
+  }
+  if (h >= 2. / 3.) {
+    b = 3. - 3. * h;
+    r = -2. + 3. * h;
+  }
+  write_rgb(
+    (int)(255. * r * l),
+    (int)(255. * g * l),
+    (int)(255. * b * l)
+  );
+}
+
+float h = 0.;
+
+void setup() {
+}
+
+void loop () {
+  write_hl(h, 1.);
+  h = fmod(h+0.01, 1);
+  delay(50);
+}
