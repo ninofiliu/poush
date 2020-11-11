9f9cceb0bddd98bf3a36949c53dff1a69b721d7f
ninofiliu <nino.filiu@gmail.com>
2020-11-11 13:26:13 +0100

    final

--- wind/wind.ino
+++ wind/wind.ino
@@ -64,21 +64,51 @@ void write_rgbw(float h, float l) {
   );
 }
 
+void interpolate(float dst[], float c, float from[], float to[]) {
+  for (int i = 0; i < 3; i++) {
+    dst[i] = (1.0 - c) * from[i] + c * to[i];
+  }
+};
+
+void interpolate_many(float *dst, float c, float *colors) {
+  int index = (int)c;
+  float sub_cursor = c - (float)index;
+  float from[3];
+  float to[3];
+  for (int i = 0; i < 3; i++) {
+    from[i] = colors[3 * index + i];
+    to[i] = colors[3 * (index + 1) + i];
+  }
+  interpolate(dst, sub_cursor, from, to);
+}
+
 // PARAMS
 
-float min_vel = 20;
-float max_vel = 150;
+float min_vel = 80;
+float max_vel = 160;
 float min_light = 0.3;
 float max_light = 0.9;
 float min_hour = 9;
 float max_hour = 24;
+float nb_colors = 5;
+float colors[18] = {
+  0.9,  0.9,  0.9,
+  1.0,  0.5,  0.0,
+  1.0,  0.3,  0.0,
+  1.0,  0.2,  0.2,
+  0.1,  0.1,  1.0,
+  0.9,  0.9,  0.9,
+};
+float hue_offset = 0.1;
 
 // GLOBALS
 
 float h = 0;
 float dir_stable = 0;
 float vel_stable = 0;
+float color[3] = {0,0,0};
 RTC_DS1307 rtc;
+float dir_debug = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -87,16 +117,19 @@ void setup() {
 }
 
 void loop() {
-  DateTime now = rtc.now();
+  /*DateTime now = rtc.now();
   if (now.hour() < min_hour || now.hour() > max_hour) {
     write_rgb(0,0,0);
     delay(10000);
     return;
-  }
+  }*/
 
   float dir = ((float)analogRead(A0))/1000;
   dir_stable = 0.9 * dir_stable + 0.1 * dir;
-  float hue = fmod(dir_stable+0.1, 1);
+  float hue = fmod(dir_stable+hue_offset, 1);
+
+  dir_debug = fmod(dir_debug+0.005, 1);
+  interpolate_many(color, nb_colors*dir_stable, colors);
 
   float vel = (float)analogRead(A1);
   vel_stable = 0.95 * vel_stable + 0.05 * vel;
@@ -104,11 +137,12 @@ void loop() {
   if (vel_stable < min_vel) { light = min_light; }
   else if (vel_stable > max_vel) { light = max_light; }
   else { light = min_light + (vel_stable-min_vel) * (max_light-min_light) / (max_vel-min_vel); }
-  
-  write_rgbw(hue, light);
-  Serial.print(100*hue);
-  Serial.print(",");
-  Serial.print(100*light);
-  Serial.println();
+
+  write_rgb(
+    (int)(255*light*color[0]),
+    (int)(255*light*color[1]),
+    (int)(255*light*color[2])
+  );
+  Serial.println(100*dir_stable);
   delay(50);
 }
