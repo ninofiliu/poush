347d2a73af23067182a352b1703a7bb476fdc287
ninofiliu <nino.filiu@gmail.com>
2020-11-11 13:40:07 +0100

    remove unused functions

--- wind/wind.ino
+++ wind/wind.ino
@@ -10,60 +10,6 @@ void write_rgb(int r, int g, int b) {
   DmxSimple.write(3, b);
 }
 
-void write_hl(float h, float l) {
-  float r = 0;
-  float g = 0;
-  float b = 0;
-  if (h < 1. / 3.) {
-    r = 1. - 3. * h;
-    g = 3. * h;
-  }
-  if (h >= 1. / 3. && h < 2. / 3.) {
-    g = 2. - 3. * h;
-    b = -1. + 3. * h;
-  }
-  if (h >= 2. / 3.) {
-    b = 3. - 3. * h;
-    r = -2. + 3. * h;
-  }
-  write_rgb(
-    (int)(255. * r * l),
-    (int)(255. * g * l),
-    (int)(255. * b * l)
-  );
-}
-
-void write_rgbw(float h, float l) {
-  float r = 0;
-  float g = 0;
-  float b = 0;
-  float w = 0;
-
-  if (h<0.25) {
-    r = 4*h;
-    w = 1-4*h;
-  } else if (h<0.5) {
-    g = 4*(h-0.25);
-    r = 4*(0.5-h);
-  } else if(h<0.75) {
-    b = 4*(h-0.5);
-    g = 4*(0.75-h);
-  } else {
-    w = 4*(h-0.75);
-    b = 4*(1-h);
-  }
-
-  r = w>r ? w : r;
-  g = w>g ? w : g;
-  b = w>b ? w : b;
-
-  write_rgb(
-    (int)(255. * r * l),
-    (int)(255. * g * l),
-    (int)(255. * b * l)
-  );
-}
-
 void interpolate(float dst[], float c, float from[], float to[]) {
   for (int i = 0; i < 3; i++) {
     dst[i] = (1.0 - c) * from[i] + c * to[i];
@@ -127,7 +73,6 @@ void loop() {
   float dir = ((float)analogRead(A0))/1000;
   dir_stable = 0.9 * dir_stable + 0.1 * dir;
   float hue = fmod(dir_stable+hue_offset, 1);
-
   interpolate_many(color, nb_colors * hue, colors);
 
   float vel = (float)analogRead(A1);
